839e78e5d8b1ca08a9f5248ade62923956428318
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-10-03 12:47:39 +0900

    Player collisions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,6 +18,22 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.Translate(Vector2.up * -1);
+        
+
+        float rayLength = 1f;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayLength, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Hitting: " + hit.collider.tag + " Name:" + hit.collider.gameObject.name);
+            //if (hit.distance)
+            //transform.Translate(Vector2.up * -1);
+        } else
+        {
+            transform.Translate(Vector2.up * -1);
+        }
+
+        Debug.DrawRay(transform.position, Vector2.down * rayLength, Color.red);
     }
 }
