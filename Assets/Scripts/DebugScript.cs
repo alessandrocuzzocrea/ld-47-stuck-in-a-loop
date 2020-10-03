d76e482127e41d179d68d695e4194f2f237e58c7
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-10-03 13:52:42 +0900

    Add debug script

---
+++ Assets/Scripts/DebugScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugScript : MonoBehaviour
+{
+    public Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 0, 50, 20), "Reset"))
+        {
+
+        }
+
+        if (GUI.Button(new Rect(10, 30, 50, 20), "Pause"))
+        {
+
+        }
+    }
+}
