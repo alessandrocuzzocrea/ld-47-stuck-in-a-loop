d9a9830a267978baa4ce420a6f26869f8d2ca1c1
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-10-04 17:11:17 +0900

    Player animations

--- Assets/Scripts/DebugScript.cs
+++ Assets/Scripts/DebugScript.cs
@@ -20,15 +20,15 @@ public class DebugScript : MonoBehaviour
 
     private void OnGUI()
     {
-        if (GUI.Button(new Rect(10, 0, 50, 20), "Reset"))
-        {
+        //if (GUI.Button(new Rect(10, 0, 50, 20), "Reset"))
+        //{
 
-        }
+        //}
 
-        if (GUI.Button(new Rect(10, 30, 50, 20), "Pause"))
-        {
+        //if (GUI.Button(new Rect(10, 30, 50, 20), "Pause"))
+        //{
 
-        }
+        //}
 
         GUI.Label(new Rect(150, 0, 400, 20), "Level ended: " + main.levelEnded);
         GUI.Label(new Rect(150, 10, 400, 20), "Current instrument: " + main.currentInstrument);
